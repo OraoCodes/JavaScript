c915b996f7490176fd24d6e1663456e54a70d56e
oraowycliffe@students.uonbi.ac.ke <oraowycliffe@students.uonbi.ac.ke>
2022-07-06 16:16:19 +0300

    querySelector manipulation

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-document.body.children[1].children[0].href = 'https://www.google.com/';
-//this can also be done
+// document.body.children[1].children[0].href = 'https://www.google.com/';
+// //this can also be done
 
-let anchorElement = document.getElementById('external-link');
-anchorElement.href = 'https://github.com/OraoCodes'
+// let anchorElement = document.getElementById('external-link');
+// anchorElement.href = 'https://github.com/OraoCodes'
+
+//this can also be done as follows
+
+let anchorElement = document.querySelector('a');
+anchorElement.href = 'https://www.linkedin.com/feed/';
