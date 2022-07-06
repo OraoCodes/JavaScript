3c75c6dbf311f41f207b95cf5ad35463718c41fd
oraowycliffe@students.uonbi.ac.ke <oraowycliffe@students.uonbi.ac.ke>
2022-07-06 15:45:34 +0300

    Alternative dom access

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 document.body.children[1].children[0].href = 'https://www.google.com/';
+//this can also be done
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://github.com/OraoCodes'
